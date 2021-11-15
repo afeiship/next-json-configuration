fbf435b7247925d843446a0ead9d2eac7a3e8722
afeiship <1290657123@qq.com>
2021-11-15 11:33:35 +0800

    feat: support multiple json

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -8,5 +8,16 @@
       store.update({ name: 'test-name' });
       expect(store.get('name')).toBe('test-name');
     });
+
+    test('load multi json', () => {
+      var store = new NxJsonConfiguration({
+        path: [
+          process.cwd() + '/__tests__/1.json',
+          process.cwd() + '/__tests__/2.json'
+        ]
+      });
+
+      expect(store.gets()).toEqual({ name: 'p2', abc: { item: 123 }, def: { item: 234 } });
+    });
   });
 })();
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
   var nx = global.nx || require('@jswork/next');
   var fs = require('fs');
   var NxAbstractConfiguration = nx.AbstractConfiguration || require('@jswork/next-abstract-configuration');
+  var nxDeepAssign = nx.deepAssign || require('@jswork/next-deep-assign');
   var DEFALUT_OPTIONS = { path: null, indent: 2, charset: 'utf8' };
 
   var NxJsonConfiguration = nx.declare('nx.JsonConfiguration', {
@@ -12,7 +13,13 @@
         return DEFALUT_OPTIONS;
       },
       load: function () {
-        return JSON.parse(fs.readFileSync(this.options.path, this.options.charset));
+        var optPath = this.options.path;
+        var charset = this.options.charset;
+        if (typeof optPath === 'string') return JSON.parse(fs.readFileSync(optPath, charset));
+        var data = optPath.map(function (path) {
+          return JSON.parse(fs.readFileSync(path, charset));
+        });
+        return nxDeepAssign.apply(null, data);
       },
       dump: function () {
         return JSON.stringify(this.data, null, this.options.indent);
